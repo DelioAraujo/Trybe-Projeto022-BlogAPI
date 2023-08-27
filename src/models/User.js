9e8aa5fa507101642203c98ee03dc3a233d83882
DelioAraujo <delioaraujo@gmail.com>
2023-08-27 15:14:53 -0300

    req2 passando

---
+++ src/models/User.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const { Model, Sequelize } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  const userModel = sequelize.define(
+    "User",
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      display_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      timestamps: false, // Desabilitar timestamps
+    }
+  );
+  return userModel;
+};
