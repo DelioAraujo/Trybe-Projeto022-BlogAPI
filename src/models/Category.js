7d65b6ed802b54b9c7a96fb86c00289c9e9c23e0
DelioAraujo <delioaraujo@gmail.com>
2023-08-29 09:54:08 -0300

    req 7 passando. pulei o 6 sem querer

---
+++ src/models/Category.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const { Model, Sequelize } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  const categoryModel = sequelize.define(
+    "Category",
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+      tableName: 'categories',
+    //   underscored: true // Desabilitar timestamps
+    }
+  );
+  return categoryModel;
+};
